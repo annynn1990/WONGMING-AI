9ed8290128519bf32c0204acbb3fc8d1e139816d
annynn1990 <annynn1990@gmail.com>
2026-09-26 22:24:46 +0800

    fix: refresh virtual human after PantherCat loader change

--- embed.js
+++ embed.js
@@ -65,7 +65,7 @@
     return null;
   })();
   var base = me ? me.src.replace(/[^/]*$/, '') : '';
-  var widgetUrl = (me && me.getAttribute('data-widget')) || (base + 'widget.html?v=20260926p2');
+  var widgetUrl = (me && me.getAttribute('data-widget')) || (base + 'widget.html?v=20260926p3');
   var savedOpen = null;
   try { savedOpen = localStorage.getItem('wm_ai_open'); } catch (e) {}
   var attrOpen = me ? me.getAttribute('data-open') : null;
